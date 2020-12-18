3badab34fa957979b45e57140768396ba30fa93e
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-18 13:31:25 +0530

    [Arti Dandge] Add : Introductory Message

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Binary Search Tree Data Structure***********");
+
+        }
+    }
+}
