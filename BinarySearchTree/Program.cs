e6833a74dc972b6dba991d5693da515e9c24b7e1
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-18 13:52:41 +0530

    [Arti Dandge] Add : Ability to create BST and add 56 as root then add 30 and 70

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,7 +7,10 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("**********Binary Search Tree Data Structure***********");
-
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
         }
     }
 }
